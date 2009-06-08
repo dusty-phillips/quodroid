8fdf058f00d0662d25229e7a366df828fe362f31
Dusty Phillips <buchuki@gmail.com>
2009-06-08 19:07:47 -0400

    Add volume commands.

--- quodroid_server/quodroid_server.py
+++ quodroid_server/quodroid_server.py
@@ -3,12 +3,18 @@ import subprocess
 from quodlibet import const
 
 urls = (
-        '/(previous|next|play-pause)/', 'QuodControl',
+        '/(previous|next|play-pause|volume-up|volume-down)/', 'QuodControl',
         )
+
+command_map = {
+        'volume-up': 'volume +',
+        'volume-down': 'volume -'
+        }
 
 class QuodControl(object):
     def GET(self, control):
         f = file(const.CONTROL, "w")
+        control = command_map.get(control, control)
         f.write(control)
         f.close()
         return(control)
