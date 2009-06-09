94b13586c459509d757d86964e837e1939487e5c
Dusty Phillips <buchuki@gmail.com>
2009-06-08 23:14:50 -0400

    fancy commenting

--- quodroid_server/quodroid_server.py
+++ quodroid_server/quodroid_server.py
@@ -11,6 +11,10 @@ command_map = {
         }
 
 class QuodControl(object):
+    '''When retrieved with a GET request, take the control parameter and pass
+    it on to quodlibet's control pipe, possibly filtering it through command
+    map to ensure the correct string is passed. Returns a status string naming
+    the current song formatted according to the STATUS_FORMAT.'''
     def GET(self, control):
         f = file(const.CONTROL, "w")
         control = command_map.get(control, control)
