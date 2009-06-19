1770673587535d5904e75b6b639658947c34bfc0
Dusty Phillips <buchuki@gmail.com>
2009-06-19 16:25:39 -0400

    Ensure settings are saved when edited.

--- quodroid/src/ca/archlinux/QuodroidSettings.java
+++ quodroid/src/ca/archlinux/QuodroidSettings.java
@@ -2,29 +2,44 @@ package ca.archlinux;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
 import android.view.KeyEvent;
 import android.view.View;
 
 public class QuodroidSettings extends Activity {
+    public static final String PREFS_NAME = "quodroid_preferences";
+    private EditText host;
+    private EditText port;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        final EditText host = (EditText) findViewById(R.id.host);
-        final EditText port = (EditText) findViewById(R.id.port );
-        SettingsListener key_down = new SettingsListener();
-        host.setOnKeyListener(key_down);
-        port.setOnKeyListener(key_down);
+        host = (EditText) findViewById(R.id.host);
+        port = (EditText) findViewById(R.id.port );
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        host.setText(settings.getString("host", "192.168.1.110"));
+        port.setText(settings.getString("port", "9250"));
+
+        SettingsListener listener = new SettingsListener();
+        host.addTextChangedListener(listener);
+        port.addTextChangedListener(listener);
     }
     
-    private class SettingsListener implements View.OnKeyListener {
-        public boolean onKey(View v, int keyCode, KeyEvent event) {
-            if (event.getAction() == KeyEvent.ACTION_DOWN) {
-              return true;
-            }
-            return false;
+    private class SettingsListener implements TextWatcher {
+        public void afterTextChanged(Editable s) {
+            SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("host", host.getText().toString());
+            editor.putString("port", port.getText().toString());
+            editor.commit();
         }
-}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+    }
 
 }
